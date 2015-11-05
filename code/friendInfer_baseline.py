3bf20577419612554bf97bb1b997a4e55cbd2637
Pr1deT <juliet.tingcheung@gmail.com>
2015-11-04 21:09:28 -0500

    Add separated features
    1. generate community features as .mat file
    2. separate friendship info

--- code/friendInfer_baseline.py
+++ code/friendInfer_baseline.py
@@ -8,6 +8,7 @@
 
 import sys
 import scipy.io as sio
+import numpy as np
 
 
 ## Feature Extraction
@@ -27,15 +28,54 @@ def feature_commu(num_ind, dataset):
 # Read data from dataset_93.mat
 def get_data(f_name):
     mat_content = sio.loadmat(f_name)
+    return mat_content
 
+## Baseline method to infer friendship, soly dependent on community label
 
+def is_friend(ind1,ind2,community):
+    #community = community.tolist()
+    if community[ind1]==community[ind2]:
+        return 1
+    else:
+        return 0
+
+def friend_matrix(community):
+    fr_mat = np.zeros(shape=(len(community),len(community)))
+
+    ind1 = 0
+    while ind1 < len(community):
+        ind2 = ind1
+        while ind2 < len(community):
+            fr_mat[ind1][ind2] = is_friend(ind1, ind2, community)
+            ind2 = ind2 + 1
+        ind1 = ind1 + 1
+
+    return fr_mat
 
 
 
 # main
 def main():
-    f_name = '../Data/dataset_93.mat'
-    dataset = get_data(f_name)
+
+    # read ground truth for friendship
+    gt_f_name = '../Data/friend_ground_truth.mat'
+    gt_mat = get_data(gt_f_name)
+    gt_key = 'friend_ground_truth'
+    ground_truth = gt_mat[gt_key]
+
+    # read community label
+    commu_f_name = '../Data/community.mat'
+    commu_mat = get_data(commu_f_name)
+    #num_ind = len(dataset)
+    commu_key = 'community'
+    community = commu_mat[commu_key]
+
+    # get baseline accuracy for friendship inference
+    fr_mat = friend_matrix(community)
+
+    print fr_mat
+
+
 
 
 if __name__ == '__main__':
--- code/get_93_dataset.m
+++ code/get_93_dataset.m
@@ -5,11 +5,13 @@ load('../Data/realitymining.mat');
 %network.sub_sort
 %dataset_93 = {}
 num_ind = 93;
+community = [];
 dataset = s(1);
 for i = 1:num_ind
     index = network.sub_sort(i);
     index
     dataset(i) = s(index);
+    community = [community;dataset(i).my_affil];
 end
 
 %save('../Data/dataset_93.mat', 'dataset', '-v7.3');
